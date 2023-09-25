a4e436c1719c0f0d652f5d786a75cfb87e99bdc2
mathman14159 <133553008+mathman14159@users.noreply.github.com>
2023-09-24 18:55:22 -0600

    Name Changes + Item Prefab

---
+++ Assets/Scripts/TriggerPickUps.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPickUps : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Item"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}

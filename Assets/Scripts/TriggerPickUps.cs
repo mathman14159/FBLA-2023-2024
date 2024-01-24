126bb8e39727d5b2161def4ac9a8dd84b0ecb48c
mathman14159 <133553008+mathman14159@users.noreply.github.com>
2024-01-24 06:46:47 -0700

    Score

--- Assets/Scripts/TriggerPickUps.cs
+++ Assets/Scripts/TriggerPickUps.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class TriggerPickUps : MonoBehaviour
 {
+    public int value;
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Item"))
         {
             Destroy(other.gameObject);
+            ScoreCounter.instance.IncreaseScore(value);
         }
         if(other.CompareTag("EnemyTop"))
         {
             Destroy(other.gameObject);
+            ScoreCounter.instance.IncreaseScore(value);
         }
     }
 }

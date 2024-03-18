b83aba8a0c77f476fd1c534629448a0445c81e07
mathman14159 <133553008+mathman14159@users.noreply.github.com>
2024-03-17 21:38:53 -0600

    Scene Transition(some how)

--- Assets/Tarodev 2D Controller/_Scripts/Scripts/OpenLvlSelect.cs
+++ Assets/Tarodev 2D Controller/_Scripts/Scripts/OpenLvlSelect.cs
@@ -5,11 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class OpenLvlSelect : MonoBehaviour
 {
+    public Animator transtion;
     // Start is called before the first frame update
    
 
     // Update is called once per frame
     public void OpenLvlSelector() {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transtion.SetTrigger("Start");
+        yield return new WaitForSeconds(1);
         SceneManager.LoadScene("Level Selection");
+
     }
 }
